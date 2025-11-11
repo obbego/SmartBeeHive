a907f783ef90d25287bc1f683bb67c16e3269fad
unknown <ciaobello@iisviolamarchesini.edu.it>
2025-11-11 13:23:29 +0100

    continued scheduler. started with function to check if it's time

--- thingsboard/scheduler/rpc-scheduler.py
+++ thingsboard/scheduler/rpc-scheduler.py
@@ -1,6 +1,7 @@
 from datetime import time, datetime
 import threading
 import subprocess
+import json
 
 class DeviceInfo:
     """Class to gather information about
@@ -23,6 +24,7 @@ class DeviceInfo:
 """Define all the constants and the variables 
 to interact with Thingsboard server"""
 THINGSBOARD_HOST_NAME = "localhost" # change this with the real host name
+JSON_FILENAME = "rpc-data.json"
 device_list = (
     DeviceInfo("device1", "dhfghsgdfhsdg"),
     DeviceInfo("device2", "dshjskldfksdfksh")
@@ -30,12 +32,12 @@ device_list = (
 
 # define the hours to do the rpc request
 # change the time if you need it
-times_device-status = {
+times_device-status = sorted({
     time(1,0)
-}
-times_weight-beehive = {
+})
+times_weight-beehive = sorted({
     time(2,0)
-}
+})
 last-time_device-status = datetime.now(), last-time_weight-beehive = datetime.now() # create variables to memorize last measure
 
 
@@ -44,4 +46,28 @@ def send_RPC_request(device, method, params):
     related to a specific device with the name of 
     the method and possible params"""
 
-    global THINGSBOARD_HOST_NAME # importing global variables
+    global THINGSBOARD_HOST_NAME, JSON_FILENAME # importing global variables
+
+    # save the method and the params in the file
+    with open(JSON_FILENAME, "w") as file:
+        content = f'{{"method":{method}, "params":{params}}}'
+        json.dump(content, file, indent=4, ensure_ascii=False) # write the converted element into the file
+  
+    # define the command to send
+    command = ["curl", "-v", "-X", "POST", f"@{JSON_FILENAME}", f"https://{THINGSBOARD_HOST_NAME}/api/v1/{device.accessToken}/rpc", "--header", "Content-Type:application/json"]
+
+    return subprocess.run(command, capture_output=True, text=True) # return the result of the rpc request
+
+def isTime(time_list, last_time):
+    """Function to check if the current time corresponds
+    to one of the times indicated, considering last time an action was done"""
+    
+    currentTime = datetime.now() # get the current datetime
+
+    for singleTime in time_list:
+        # CONDITION TO BE FINISHED
+        if currentTime.time() >= singleTime and datetime.combine(datetime.now().date(), singleTime):
+            pass
+
+def weightBeehiveScheduler():
+    pass
