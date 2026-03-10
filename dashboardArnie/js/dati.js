a11e4a3cb0a050e9bcd2605b008fb937d2192060
docente1 <docente1@internal.iisviolamarchesini.edu.it>
2026-03-10 12:22:41 +0100

    restore button demo

--- dashboardArnie/js/dati.js
+++ dashboardArnie/js/dati.js
@@ -1,6 +1,6 @@
 // js/data.js
 const hivesData = [
-  { id: 1, name: 'Arnia 01', t: 0.0, tOut: 0, w: 0, h: 0, pct: 0, status: 'green', lastUpdate: '10 min fa' },
+  { id: 1, name: 'Arnia 01', t: 37.1, tOut: 34, w: 51.2, h: 34, pct: 54, status: 'green', lastUpdate: '10 min fa' },
   { id: 2, name: 'Arnia 02', t: 42.1, tOut: 36, w: 43.8, h: 24, pct: 40, status: 'red', lastUpdate: '18 ore fa' },
   { id: 3, name: 'Arnia 03', t: 35.8, tOut: 37, w: 46.5, h: 58, pct: 88, status: 'yellow', lastUpdate: '1 ora fa' },
   { id: 4, name: 'Arnia 04', t: 33.5, tOut: 39, w: 38.3, h: 54, pct: 15, status: 'red', lastUpdate: '2 giorni fa' },
