00b39a277a9151e461c150f64a4fd5cb77c93d4d
danielechiarion <dnaiele.chiarion@iisviolamarchesini.edu.it>
2026-03-26 18:26:10 +0100

    add rule chain connected to checking validity of the telemetry key

--- test/request-http/post-telemetry.sh
+++ test/request-http/post-telemetry.sh
@@ -1,6 +1,6 @@
 DEVICE_ACCESS_TOKEN=BhP1QobnPngCTa3qjo6Z
 THINGSBOARD_HOST_NAME=demo.thingsboard.io
-DATA_FILE=rpc-data.json
+DATA_FILE=telemetry-data.json
 
 response=$(curl -v -X POST -d @"$DATA_FILE" https://"$THINGSBOARD_HOST_NAME"/api/v1/"$DEVICE_ACCESS_TOKEN"/telemetry --header "Content-Type:application/json")
 echo "$response"
