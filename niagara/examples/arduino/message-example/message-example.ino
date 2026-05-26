645dcc147fd85ff0ede9741d8ca1a8294a0530f6
Enrico Zanotti <enrico.zanotti@iisviolamarchesini.edu.it>
2026-05-26 13:16:52 +0200

    update rule chain replacing const with var. to be finished

--- niagara/examples/arduino/message-example/message-example.ino
+++ niagara/examples/arduino/message-example/message-example.ino
@@ -4,6 +4,15 @@
 
 Niagara *lora_device;
 
+double randomDouble(double minVal, double maxVal) {
+  // random() restituisce un long intero
+  // Dividendo per 1.000.000 otteniamo una frazione tra 0.0 e 1.0
+  double r = (double)random(0, 1000000) / 1000000.0;
+
+  // Scala il valore nel range desiderato
+  return minVal + r * (maxVal - minVal);
+}
+
 void setup() {
     Serial.begin(115200);
     while (!Serial) { delay(10); }
@@ -17,17 +26,14 @@ void setup() {
 void loop() {
     Serial.println("\n[INFO] Preparazione nuove misurazioni...");
 
-    NiagaraMeasure temp("temperature", 24.8);
-    NiagaraMeasure hum("humidity", 58.3);
-
-    // Misura con timestamp CUSTOM
-    unsigned long tempo_misura_passata = millis() - 5000;
-    NiagaraMeasure press("pressure", 1012.4, tempo_misura_passata);
+    NiagaraMeasure temp("tempIn", randomDouble(15, 25));
+    NiagaraMeasure hum("humidity", randomDouble(20, 80));
+    NiagaraMeasure weight("weight", randomDouble(35, 60));
+    NiagaraMeasure freq("peakFreq", randomDouble(5000, 15000));
 
     // Inizializziamo il Sender passando il riferimento all'unica istanza 'lora_device'
     // Usiamo il costruttore variadico per inserire subito le prime due misure
-    NiagaraSender sender(*lora_device, temp, hum);
-    sender.add_measure(press);
+    NiagaraSender sender(*lora_device, temp, hum, weight, freq);
 
     Serial.println("[LORA] Invio dati al Gateway...");
     
